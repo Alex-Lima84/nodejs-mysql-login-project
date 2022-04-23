90b9888220e073c87599b832ba68855570a98fd5
Alexandre L C Lima <alexandre.cerutti@live.com>
2022-04-23 17:22:25 -0300

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const authController = require('../controllers/auth');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.render('index');
+})
+
+router.post('/register', authController.register );
+
+router.post('/login', authController.login);
+
+router.get('/logout', authController.logout);
+
+module.exports = router;
